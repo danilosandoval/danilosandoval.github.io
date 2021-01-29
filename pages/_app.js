ff6d33fc7475355523a99b9dabe4d71324baccf0
danilosandoval-root <root@danilosandoval.com>
2021-01-29 14:57:17 -0300

    CNAME e inicios de maquetado

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Head from 'next/head'
+
 import "tailwindcss/tailwind.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <script src="https://kit.fontawesome.com/4476de7b7c.js" crossorigin="anonymous"></script>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,33 @@
 const Home = () => {
   return (
-    <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-        <div class="sm:text-center lg:text-left">
-          <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span class="block xl:inline">Hi everyone! </span>
-            <span class="block text-indigo-600 xl:inline">I'm Danilo Sandoval</span>
+    <div>
+      <header class="flex mt-10 mx-auto max-w-7xl px-4 sm:mt-10 sm:px-6 md:mt-10 lg:mt-10 lg:px-8 xl:mt-10">
+        <div class="flex-grow sm:text-center lg:text-left">
+          <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-4xl">
+            <span class="block xl:inline">Danilo Sandoval</span>
           </h1>
-          <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+          <p class="mt-1 text-base text-gray-600 sm:mt-1 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-1 md:text-xl lg:mx-0">
+            Ninja Developer
           </p>
-          <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-            <div class="rounded-md shadow">
-              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                Contact me!
-              </a>
-            </div>
-            <div class="mt-3 sm:mt-0 sm:ml-3">
-              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
-                Follow Me!
-              </a>
-            </div>
+        </div>
+        <div class="flex-none sm:flex sm:justify-center lg:justify-start">
+          <div class="sm:mt-0 sm:ml-3">
+            <a href="#" class="w-full flex items-center justify-center px-2 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 md:py-3 md:text-lg md:px-5">
+              <i class="fab fa-github mr-2"></i> danilosandoval-root
+            </a>
+          </div>
+          <div class="sm:mt-0 sm:ml-3">
+            <a href="#" class="w-full flex items-center justify-center px-2 py-2 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-3 md:text-lg md:px-5">
+              <i class="fab fa-linkedin mr-2"></i> Danilo Sandoval
+            </a>
           </div>
         </div>
+      </header>
+      <main>
       </main>
+      <footer>
+      </footer>
+    </div>
   )
 }
 
