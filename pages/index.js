8d6f993c0699467bc1748df93474073b22c3e0b0
danilosandoval-root <root@danilosandoval.com>
2021-01-29 18:16:41 -0300

    Avance en la welcome page

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,151 @@
+import React from 'react'
+
 const Home = () => {
+
+  const hiArr = [
+    "Asalaam alaikum",
+    "Zdraveĭte",
+    "Nǐn hǎo",
+    "Goede dag",
+    "Hello",
+    "Bonjour",
+    "Dia dhuit",
+    "Guten tag",
+    "Kalimera",
+    "Shalom aleichem",
+    "Namastē",
+    "Góðan dag",
+    "Selamat siang",
+    "CIao",
+    "Salve",
+    "Konnichiwa",
+    "Suostei",
+    "Anyoung haseyo",
+    "Cześć",
+    "Dzień dobry!",
+    "Olá",
+    "Bună ziua",
+    "Zdravstvuyte",
+    "Hola",
+    "Habari",
+    "God dag",
+    "ora na	Ia ora na",
+    "Merhaba",
+    "Xin chào",
+    "Shwmae",
+    "Ngiyakwemukela"
+  ]
+
+  const [index, setIndex] = React.useState( Math.floor(Math.random() * (hiArr.length - 0) + 0) )
+  const [msg, setMsg] = React.useState( hiArr[index] )
+
+  React.useEffect(() => {
+    const repeat = setInterval(() => { 
+      let i = Math.floor(Math.random() * (hiArr.length - 0) + 0)
+      setIndex(i)
+      setMsg( hiArr[i] ) 
+    }, 3000);
+    return () => clearInterval(repeat)
+  }, [])
+
+
   return (
-    <div>
-      <header class="flex mt-10 mx-auto max-w-7xl px-4 sm:mt-10 sm:px-6 md:mt-10 lg:mt-10 lg:px-8 xl:mt-10">
-        <div class="flex-grow sm:text-center lg:text-left">
-          <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-4xl">
-            <span class="block xl:inline">Danilo Sandoval</span>
-          </h1>
-          <p class="mt-1 text-base text-gray-600 sm:mt-1 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-1 md:text-xl lg:mx-0">
-            Ninja Developer
-          </p>
-        </div>
-        <div class="flex-none sm:flex sm:justify-center lg:justify-start">
-          <div class="sm:mt-0 sm:ml-3">
-            <a href="#" class="w-full flex items-center justify-center px-2 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 md:py-3 md:text-lg md:px-5">
-              <i class="fab fa-github mr-2"></i> danilosandoval-root
-            </a>
+    <div class="relative bg-white overflow-hidden">
+      <div class="max-w-7xl mx-auto">
+        <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
+
+          <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+            <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
+              <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                <div class="flex items-center justify-between w-full md:w-auto">
+                  <a href="#">
+                    <span class="sr-only">Workflow</span>
+                    <a href="#" class="w-full flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 md:py-3 md:text-lg md:px-4">
+                      DS
+                    </a>
+                  </a>
+                  <div class="-mr-2 flex items-center md:hidden">
+                    <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" id="main-menu" aria-haspopup="true">
+                      <span class="sr-only">Open main menu</span>
+                      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                <a href="#" class="font-medium text-gray-500 hover:text-gray-900"><i class="fab fa-github"></i></a>
+
+                <a href="#" class="font-medium text-gray-500 hover:text-gray-900"><i class="fab fa-twitter"></i></a>
+
+                <a href="#" class="font-medium text-gray-500 hover:text-gray-900"><i class="fab fa-linkedin"></i></a>
+
+                <a href="#" class="font-medium text-gray-500 hover:text-gray-900"><i class="fas fa-envelope"></i></a>
+              </div>
+            </nav>
           </div>
-          <div class="sm:mt-0 sm:ml-3">
-            <a href="#" class="w-full flex items-center justify-center px-2 py-2 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-3 md:text-lg md:px-5">
-              <i class="fab fa-linkedin mr-2"></i> Danilo Sandoval
-            </a>
+          <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+            <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+              <div class="px-5 pt-4 flex items-center justify-between">
+                <div>
+                  <a href="#" class="w-full flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 md:py-3 md:text-lg md:px-4">
+                    DS
+                  </a>
+                </div>
+                <div class="-mr-2">
+                  <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span class="sr-only">Close main menu</span>
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <div role="menu" aria-orientation="vertical" aria-labelledby="main-menu">
+                <div class="px-2 pt-2 pb-3 space-y-1" role="none">
+                  <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Github</a>
+
+                  <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Twitter</a>
+
+                  <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Linkedin</a>
+
+                  <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Contacto</a>
+                </div>
+              </div>
+            </div>
           </div>
+          <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div class="sm:text-center lg:text-left">
+              <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <span class="block xl:inline">{msg}!</span>
+                <span class="block text-indigo-600 xl:inline"> @danilosandoval</span>
+              </h1>
+              <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+              </p>
+              <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                <div class="mt-3 sm:mt-0 sm:ml-3">
+                  <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                    Get started
+                  </a>
+                </div>
+                <div class="mt-3 sm:mt-0 sm:ml-3">
+                  <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                    Live demo
+                  </a>
+                </div>
+              </div>
+            </div>
+          </main>
         </div>
-      </header>
-      <main>
-      </main>
-      <footer>
-      </footer>
+      </div>
+      <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="" />
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Poppins', 'system-ui', 'sans-serif'],
+      body: ['Poppins', 'system-ui', 'sans-serif'],
+    },
+    extend: {
+    },
   },
   variants: {
     extend: {},
